30b7b87c090c29e25df8d12e5b198db917e40024
ephraimjensen <jen22082@byui.edu>
2024-01-10 12:33:27 -0700

    Completed Prep 1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        string fName;
+        string lName;
+
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your first name? ");
+        fName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        lName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lName}, {fName} {lName}");
+
     }
 }
