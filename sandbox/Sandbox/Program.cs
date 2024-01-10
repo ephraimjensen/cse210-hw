b9972001ccf85bdea32db2f61e11a988368e6244
ephraimjensen <jen22082@byui.edu>
2024-01-10 12:16:25 -0700

    Played in the sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello Sandbox World!");
+        int x;
+        x = 5;
+        // Console.WriteLine(x);
+        int y = 4;
+        // Console.WriteLine(x * y);
+        // Console.WriteLine("Hello world");
+        // Console.Write("There will not be a new line after this.");
+
+        // Console.WriteLine(" This adds a new line to offset the previous one.");
+        // Console.Write("What is your favorite color? ");
+        // string color = Console.ReadLine();
+        // Console.WriteLine($"Your favorite color is {color}! ");
+        if (x > y)
+        {
+            Console.WriteLine(" x is greater than y");
+
+        }
     }
 }
