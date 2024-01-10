1df9922cfc9a215adc1c24fbe1b1a1a1918e103e
ephraimjensen <jen22082@byui.edu>
2024-01-10 12:51:15 -0700

    sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,31 +5,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Console.WriteLine("Hello Sandbox World!");
-        int x;
-        x = 5;
-        // Console.WriteLine(x);
-        int y = 4;
-        // Console.WriteLine(x * y);
-        // Console.WriteLine("Hello world");
-        // Console.Write("There will not be a new line after this.");
+        // // Console.WriteLine("Hello Sandbox World!");
+        // // int x;
+        // // x = 5;
+        // // // Console.WriteLine(x);
+        // // int y = 4;
+        // // // Console.WriteLine(x * y);
+        // // // Console.WriteLine("Hello world");
+        // // // Console.Write("There will not be a new line after this.");
 
-        // Console.WriteLine(" This adds a new line to offset the previous one.");
-        // Console.Write("What is your favorite color? ");
-        // string color = Console.ReadLine();
-        // Console.WriteLine($"Your favorite color is {color}! ");
-        if (x > y)
-        {
-            Console.WriteLine(" x is greater than y");
+        // // // Console.WriteLine(" This adds a new line to offset the previous one.");
+        // // // Console.Write("What is your favorite color? ");
+        // // // string color = Console.ReadLine();
+        // // // Console.WriteLine($"Your favorite color is {color}! ");
+        // // if (x > y)
+        // // {
+        // //     Console.WriteLine(" x is greater than y");
 
-        }
-        int number = 5;
-        number = number + 3;
-        if (number > 3)
-        {
-            Console.WriteLine("Greater");
-        }
+        // // }
+        // // int number = 5;
+        // // number = number + 3;
+        // // if (number > 3)
+        // // {
+        // //     Console.WriteLine("Greater");
+        // // }
+        // int x = 6;
+        // int y = 7;
+        // int z = 4;
 
+        // if (x > y)
+        // {
+        //     Console.WriteLine("greater than y");
+        // }
+
+        // else if (x > z)
+        // {
+        //     Console.WriteLine("greater than z");
+        // }
+
+        // else
+        // {
+        //     Console.WriteLine("less than both y and z");
+        // }
+        string valueInText = "42";
+        int number = int.Parse(valueInText);
 
 
     }
