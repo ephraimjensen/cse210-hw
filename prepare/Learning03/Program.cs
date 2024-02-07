79d6cacd79eab4af06d24717ce40c6ad8cf9a37c
ephraimjensen <jen22082@byui.edu>
2024-02-07 14:15:26 -0700

    completed learning activity 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        var fraction1 = new Fraction();
+        var fraction2 = new Fraction(6);
+        var fraction3 = new Fraction(6, 7);
+        var fraction4 = new Fraction(7, 6);
+
+        Console.WriteLine(fraction1.GetDecimalFraction());
+        Console.WriteLine(fraction1.GetFractionString());
+
+        Console.WriteLine(fraction2.GetDecimalFraction());
+        Console.WriteLine(fraction2.GetFractionString());
+
+        Console.WriteLine(fraction3.GetDecimalFraction());
+        Console.WriteLine(fraction3.GetFractionString());
+
+        Console.WriteLine(fraction4.GetDecimalFraction());
+        Console.WriteLine(fraction4.GetFractionString());
+
+
     }
 }
